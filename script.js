b70d771b8608809caded26250529781905f54f20
Devesh Shukla <deveshshukla1603@gmail.com>
2023-08-23 15:57:49 +0530

    Completed

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Creating a new Date object
 let currentDate = new Date();
+const weekDay = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
 // Getting different components of the date
 const year = currentDate.getFullYear();
 const month = currentDate.getMonth(); // 0-indexed (0 - January, 11 - December)
@@ -7,9 +9,12 @@ const day = currentDate.getDate();
 
 let mon = month+1;
 if(mon<11) mon = "0"+mon;
+
 document.getElementById('year').textContent = year;
 document.getElementById('month').textContent = mon;
 document.getElementById('date').textContent = day;
+document.getElementById('day').textContent = weekDay[currentDate.getDay()];
+
 function update() {
     const current = new Date();
     const hours = current.getHours();
@@ -23,7 +28,7 @@ function update() {
     document.getElementById('hr').textContent = formattedHours;
     document.getElementById('min').textContent = formattedMinutes;
     document.getElementById('sec').textContent = formattedSeconds;
-    console.log("updates",seconds);
+    // console.log("updates",seconds);
 }
 update();
 setInterval(update, 1000);
